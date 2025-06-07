7d6d6bb047e152ff5b1efe68e8ece313322e7d61
Rahul Wani <118592080+Raahulwani@users.noreply.github.com>
2025-06-07 12:03:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-rahul-123"
+    bucket = "mydev-tf-state-bucket-project-terraform-rahul-1234"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table1"
